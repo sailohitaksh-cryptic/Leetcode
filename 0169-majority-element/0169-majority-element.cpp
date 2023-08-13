3a166a6c44501286aa5b0ec83129771fb473b651
Sai Lohitaksh Reddy D <74712527+sailohitaksh-cryptic@users.noreply.github.com>
2023-08-13 14:47:50 +0530

    Time: 15 ms (82.51%), Space: 19.6 MB (57.12%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -15,10 +15,10 @@ public:
                 count=1;
             }
         }
-        count=0;
+        /*count=0;
         for(int i=0;i<n;i++)
             if(nums[res]==nums[i])
-                count++;
+                count++;*/
         return nums[res];
     }
 };
