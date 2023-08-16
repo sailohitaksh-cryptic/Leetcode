283d9a86c6f067a1d2b8c0bc699d5b366dd5d98c
Sai Lohitaksh Reddy D <74712527+sailohitaksh-cryptic@users.noreply.github.com>
2023-08-16 16:43:21 +0530

    Time: 207 ms (86.58%), Space: 134.8 MB (36.59%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int>v;
-        list<int>q;
-        int i=0;
-        int j=0;
-        while(j<nums.size()){
-            while(q.size()>0&&q.back()<nums[j]){
-                cout<<q.back()<<" ";
-                q.pop_back();
-            }
-            q.push_back(nums[j]);
-              if(j-i+1==k){
-                v.push_back(q.front());
-                if(q.front()==nums[i]){
-                    q.pop_front();
-                }
-                i++;
-            }
-            j++;
-        }
-        return v;
+        vector<int> result;
+    if (k == 0) return result;
+    deque<int> w;
+    for (int i = 0, n = (int)nums.size(); i < n; i++) {
+        // If the pq head in out of the window --> pop head
+        while (!w.empty() && w.front() <= i-k)
+            w.pop_front();
+        // If mq is non empty and current val >= last element of mq
+        // --> remove all elements that are <= from back 
+        while (!w.empty() && nums[w.back()] <= nums[i])
+            w.pop_back();
+        // Takes care of inserting into the end
+        w.push_back(i);
+        // Store results from the end of the first window
+        if (i >= k-1)
+            result.push_back(nums[w.front()]);
+    }
+    return result;
         
     }
 };
